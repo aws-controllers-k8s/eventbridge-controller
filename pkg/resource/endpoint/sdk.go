a5356794fd79ba4de8ca210fa533cb482ef2adb8
ci-robot <ci-robot@k8s.io>
2023-03-07 21:37:47 +0000

    Update to ACK runtime `v0.24.1`, code-generator `v0.24.3`

--- pkg/resource/endpoint/sdk.go
+++ pkg/resource/endpoint/sdk.go
@@ -77,6 +77,9 @@ func (rm *resourceManager) sdkFind(
 	resp, err = rm.sdkapi.DescribeEndpointWithContext(ctx, input)
 	rm.metrics.RecordAPICall("READ_ONE", "DescribeEndpoint", err)
 	if err != nil {
+		if reqErr, ok := ackerr.AWSRequestFailure(err); ok && reqErr.StatusCode() == 404 {
+			return nil, ackerr.NotFound
+		}
 		if awsErr, ok := ackerr.AWSError(err); ok && awsErr.Code() == "ResourceNotFoundException" {
 			return nil, ackerr.NotFound
 		}
@@ -389,7 +392,7 @@ func (rm *resourceManager) sdkUpdate(
 		return latest, requeueWaitWhileUpdating
 	}
 
-	input, err := rm.newUpdateRequestPayload(ctx, desired)
+	input, err := rm.newUpdateRequestPayload(ctx, desired, delta)
 	if err != nil {
 		return nil, err
 	}
@@ -491,6 +494,7 @@ func (rm *resourceManager) sdkUpdate(
 func (rm *resourceManager) newUpdateRequestPayload(
 	ctx context.Context,
 	r *resource,
+	delta *ackcompare.Delta,
 ) (*svcsdk.UpdateEndpointInput, error) {
 	res := &svcsdk.UpdateEndpointInput{}
 
